b8ae232d8c52ea5553a319fa459604d0c12974d0
gustavo <pfeifer.g@gmail.com>
2019-10-22 19:59:44 -0300

    Add spring boots projects (ms-users, ms-products and ms-admin)

---
+++ ms-admin/src/test/java/com/ggp/msadmin/MsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggp.msadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
